b4f6f6df9797017b4d9f57500ddd71d4b787b8e5
Ayberk Keskin <81094165+Ayberkeskin@users.noreply.github.com>
2023-08-18 19:18:45 +0300

    +Animating The Player

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
@@ -8,12 +9,16 @@ public class PlayerController : MonoBehaviour
 
     Rigidbody2D rb;
     PlayerControls playerControls;
+    Animator myAnimator;
+    SpriteRenderer mySpriteRenderer;
     Vector2 movement;
 
     private void Awake()
     {
         playerControls = new PlayerControls();
         rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void OnEnable()
@@ -27,16 +32,33 @@ public class PlayerController : MonoBehaviour
     }
     private void FixedUpdate()
     {
+        AdjustPlayerFacingDirection();
         Move();
     }
 
     private void PlayerInput()
     {
         movement=playerControls.Movement.Move.ReadValue<Vector2>();
+
+        myAnimator.SetFloat("moveX",movement.x);
+        myAnimator.SetFloat("moveY",movement.y);
+
     }
 
     private void Move()
     {
         rb.MovePosition(rb.position+(movement*moveSpeed*Time.fixedDeltaTime));
+    }
+
+    private void AdjustPlayerFacingDirection()
+    {
+        Vector3 mousePos=Input.mousePosition;
+        Vector3 playerScreePoint=Camera.main.WorldToScreenPoint(transform.position);
+
+        if (mousePos.x<playerScreePoint.x)
+            mySpriteRenderer.flipX = true;
+        else
+            mySpriteRenderer.flipX = false;
+
     }
 }
