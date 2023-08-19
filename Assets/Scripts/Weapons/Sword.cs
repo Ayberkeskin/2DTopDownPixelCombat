d940afa98025c79f3770380f61a445ec185b5810
Ayberk Keskin <81094165+Ayberkeskin@users.noreply.github.com>
2023-08-19 19:31:25 +0300

    +Swing Both Directions

--- Assets/Scripts/Weapons/Sword.cs
+++ Assets/Scripts/Weapons/Sword.cs
@@ -6,8 +6,12 @@ public class Sword : MonoBehaviour
 {
     private PlayerControls playerControls;
     private Animator myAnimator;
+    private PlayerController playerController;
+    private ActiveWeapon activeWeapon;
     private void Awake()
     {
+        playerController=GetComponentInParent<PlayerController>();
+        activeWeapon = GetComponentInParent<ActiveWeapon>();
         myAnimator = GetComponent<Animator>();  
         playerControls= new PlayerControls();
     }
@@ -21,8 +25,24 @@ public class Sword : MonoBehaviour
         playerControls.Combat.Attack.started += _ => Attack();
     }
 
+    private void Update()
+    {
+        MouseFollowWithOffset();
+    }
     private void Attack()
     {
         myAnimator.SetTrigger("Attack");
+    }
+    private void MouseFollowWithOffset()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerScreePoint = Camera.main.WorldToScreenPoint(playerController.transform.position);
+
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x)*Mathf.Rad2Deg;
+
+        if (mousePos.x< playerScreePoint.x)
+            activeWeapon.transform.rotation = Quaternion.Euler(0, -180, angle);
+        else
+            activeWeapon.transform.rotation = Quaternion.Euler(0, 0, angle);
     }
 }
